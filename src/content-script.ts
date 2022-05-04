ef2f96f81418d62c01bed0a0ac7a579f63cdbe7d
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-04 13:19:17 +0900

    hide loaded contents

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,18 +1,20 @@
 const drawer = document.querySelector('#guide-content')
+const contents = document.querySelector('#contents')
 const config = {attributes: false, childList: true, subtree: true}
 
-const cb = function (_, __) {
-    const sections = document.querySelector('#sections')
-    if (sections == null || sections.children.length <= 1) {
-        return;
-    }
-    const channels = sections.children[1].querySelectorAll('#endpoint')
-    const links = [];
-    channels.forEach((e) =>
-        links.push(e.getAttribute('href')));
+let contentsLength = 0
+let links = []
 
+function bleach() {
+    if (links.length <= 0) {
+        return
+    }
     const contents = document.querySelector('#contents')
     const list = contents.querySelectorAll('#content')
+    if (list.length == contentsLength) {
+        return
+    }
+    contentsLength = list.length
     list.forEach((e) => {
         const link = e.querySelector('#avatar-link')
         const href = link.getAttribute('href')
@@ -22,5 +24,18 @@ const cb = function (_, __) {
     })
 }
 
-const observer = new MutationObserver(cb)
-observer.observe(drawer, config)
+new MutationObserver((_, __) => {
+    const sections = document.querySelector('#sections')
+    if (sections == null || sections.children.length <= 1) {
+        return;
+    }
+    const channels = sections.children[1].querySelectorAll('#endpoint')
+    channels.forEach((e) =>
+        links.push(e.getAttribute('href')))
+
+    bleach()
+}).observe(drawer, config)
+
+new MutationObserver((_, __) => {
+    bleach()
+}).observe(contents, config)
