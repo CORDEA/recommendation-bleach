8c14076ad2d2104e7c7cfb11ffa90e0c0d9dd899
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-04 13:46:01 +0900

    add button to show hidden content

--- src/content-script.ts
+++ src/content-script.ts
@@ -5,6 +5,17 @@ const config = {attributes: false, childList: true, subtree: true}
 let contentsLength = 0
 let links = []
 
+function createShowButton(): HTMLButtonElement {
+    const button = document.createElement('button')
+    button.setAttribute('id', 'rb-show')
+    button.setAttribute(
+        'style',
+        'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);'
+    )
+    button.innerHTML = 'Show'
+    return button;
+}
+
 function bleach() {
     if (links.length <= 0) {
         return
@@ -19,7 +30,16 @@ function bleach() {
         const link = e.querySelector('#avatar-link')
         const href = link.getAttribute('href')
         if (links.indexOf(href) < 0) {
-            e.setAttribute('style', 'display: none;')
+            const container = e.querySelector('#dismissible')
+            container.setAttribute('style', 'display: none;')
+            if (e.querySelector('#rb-show') == null) {
+                const button = createShowButton()
+                button.addEventListener('click', () => {
+                    container.removeAttribute('style')
+                    e.removeChild(button)
+                })
+                e.appendChild(button)
+            }
         }
     })
 }
